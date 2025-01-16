f13e1c6fdb586c5aa547adb3ba3b7ff7c3d4d8ff
cuongbd <bdcuongvn83git@gmail.com>
2025-01-16 21:34:57 +1100

    add feature: Web API for student service

---
+++ Data/WebAPIPortalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIPortal.Models;
+
+namespace WebAPIPortal.Data
+{
+    public class WebAPIPortalContext : DbContext
+    {
+        public WebAPIPortalContext (DbContextOptions<WebAPIPortalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIPortal.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace WebAPIPortal.Models
+{
+    [Table("STUDENT")]
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        
+        public DateTime EnrollmentDate { get; set; }
+
+        public string FullName
+        {
+            get { return FirstMidName + "_" + LastName; }
+        }
+
+        
+    }
+}
